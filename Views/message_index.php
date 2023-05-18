842337f8c80684d611f3fed1ff81526e6d1bca5b
hery <heriniaina.eugene@gmail.com>
2023-05-18 19:45:02 +0200

    Add search in message list

--- Views/message_index.php
+++ Views/message_index.php
@@ -1,5 +1,6 @@
 <?php $this->extend(getenv('serasera.layout')) ?>
 <?php $this->section('main'); ?>
+<?php $request = service('request'); ?>
 <h3 class="my-3">
     <?php echo $page_title; ?>
 </h3>
@@ -7,6 +8,50 @@
     <?php echo $page_description; ?>
 </div>
 
+<div class="mb-3">
+    <form method="get">
+        <input type="hidden" id="expand" name="x" value="<?php echo $request->getGet('x') ?? '0' ?>">
+        <div class="d-flex">
+            <div class="p-2 flex-fill">
+                <input type="text" class="form-control" name="title" id="title"
+                    placeholder="<?php echo lang('Forum.in_title') ?>"
+                    value="<?php echo $request->getGet('title') ?>">
+            </div>
+            <div class="p-2 flex-fill">
+                <input type="text" class="form-control" name="username" id="username"
+                    placeholder="<?php echo lang('Forum.in_author') ?>"
+                    value="<?php echo $request->getGet('username') ?>">
+            </div>
+            <div class="p-2 flex-shrink"><button type="submit" id="btn-submit" class="btn btn-primary rounded"><i
+                        class="bi-search"></i></button></div>
+            <div class="p-2 flex-shrink"><button type="button" id="btn-expand" class="btn btn-primary rounded"><i
+                        class="bi-plus"></i></button></div>
+        </div>
+
+        <div id="search-more" <?php echo (($request->getGet('x') != 1) ? 'style="display: none;"' : '') ?>>
+            <div class="p-2 flex-fill">
+                <div class="row align-items-center">
+                    <div class="col-md-7">
+                        <textarea class="form-control" name="message" id="message" rows="1"
+                            placeholder="<?php echo lang('Forum.in_message') ?>"><?php echo $request->getGet('message') ?></textarea>
+                    </div>
+                    <div class="col-sm-5 col-md-2">
+                        <label for="sort">
+                            <?php echo lang('Forum.message_order') ?>
+                        </label>
+                    </div>
+                    <div class="col-sm-7 col-md-3">
+                        <select class="form-select" name="sort" id="sort">
+                            <option value='id' <?php echo ($request->getGet('sort') == 'id') ? 'selected' : '' ?>><?php echo lang('Forum.by_date') ?></option>
+                            <option value="title" <?php echo ($request->getGet('sort') == 'title') ? 'selected' : '' ?>><?php echo lang('Forum.by_title') ?></option>
+                            <option value="author" <?php echo ($request->getGet('sort') == 'author') ? 'selected' : '' ?>><?php echo lang('Forum.by_author') ?></option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
 <?php foreach ($messages as $row) { ?>
     <?php //fix last 
         if ($row['last_mid'] == '')
@@ -38,7 +83,7 @@
                 </div>
                 <div class="">
                     <?php echo substr(strip_tags($row['message']), 0, 200) ?>...
-                    <?php echo anchor('forum/message/' . $row['last_mid'] , lang('Forum.more')) ?>
+                    <?php echo anchor('forum/message/' . $row['last_mid'], lang('Forum.more')) ?>
                 </div>
             </div>
             <?php if ($row['last_date'] != $row['date']) { ?>
@@ -54,4 +99,20 @@
     <?php echo $pager->links() ?>
 </div>
 
+<?php $this->endSection(); ?>
+<?php $this->section('scripts') ?>
+<script>
+    $(function() {
+        $("#btn-expand").click(function(expand) {
+            $('#search-more').toggle("slow", function() {
+                if ($(this).is(":visible")) {
+                    $('#expand').attr('value', '1');
+                } else {
+                    $('#expand').attr('value', '0');
+                }
+            });
+
+        });
+    });
+</script>
 <?php $this->endSection(); ?>
